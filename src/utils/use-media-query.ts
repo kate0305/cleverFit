21d826655a295a4d36dae1d252d22971ad49332c
Katsiaryna Voronich <katarinka403@gmail.com>
2024-02-10 02:34:55 +0300

    feat: implement hook to check media query

---
+++ src/utils/use-media-query.ts
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+export const useMediaQuery = (query: string) => {
+    const [matches, setMatches] = useState(false);
+    useEffect(() => {
+        const matchQueryList = window.matchMedia(query);
+        console.log(query);
+        setMatches(matchQueryList.matches);
+        const handleChange = (e: MediaQueryListEvent) => setMatches(e.matches);
+        matchQueryList.addEventListener('change', handleChange);
+        return () => {
+            matchQueryList.removeEventListener('change', handleChange);
+        };
+    }, [query]);
+    return matches;
+};
