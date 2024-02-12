583d914338104304475ab3782db8b823e10ccff1
Katsiaryna Voronich <katarinka403@gmail.com>
2024-02-12 15:38:06 +0300

    refactor: update data and styles for navbar

--- src/components/navbar/data.tsx
+++ src/components/navbar/data.tsx
@@ -1,71 +1,45 @@
 import { CalendarTwoTone, HeartFilled, TrophyFilled, IdcardOutlined } from '@ant-design/icons';
-import { MenuProps } from 'antd';
+
 import styles from './navbar.module.scss';
+import { MenuItem, getListItemWithIcon } from '@utils/get-list-item-with-icon';
 
 const data = [
     {
         key: '1',
-        icon: CalendarTwoTone,
+        icon: (
+            <CalendarTwoTone style={{ fontSize: '14px' }} twoToneColor={['#061178', '#061178']} />
+        ),
         label: 'Календарь',
     },
     {
         key: '2',
-        icon: HeartFilled,
+        icon: <HeartFilled style={{ color: '#061178', fontSize: '14px' }} />,
         label: 'Тренировки',
     },
     {
         key: '3',
-        icon: TrophyFilled,
+        icon: <TrophyFilled style={{ color: '#061178', fontSize: '14px' }} />,
         label: 'Достижения',
     },
     {
         key: '4',
-        icon: IdcardOutlined,
+        icon: <IdcardOutlined style={{ color: '#061178', fontSize: '14px' }} />,
         label: 'Профиль',
     },
 ];
-
-type MenuItem = Required<MenuProps>['items'][number];
-
-function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    style?: React.CSSProperties | undefined,
-    className?: string,
-    children?: MenuItem[],
-    type?: 'group',
-): MenuItem {
-    return {
-        key,
-        icon,
-        className,
-        style,
-        children,
-        label,
-        type,
-    } as MenuItem;
-}
 
 export const createMenuItemsArr = (
     isWidthChanged: boolean,
     isClosedSidebar: boolean,
 ): MenuItem[] => {
     const arr = data.map(({ key, icon, label }) => {
-        const Icon = () => {
-            const IconComponent = icon;
-            return !isWidthChanged ? (
-                <IconComponent
-                    style={{ color: '#061178', fontSize: '14px' }}
-                    twoToneColor='#061178'
-                />
-            ) : null;
-        };
+        const Icon = () => (!isWidthChanged ? icon : null);
+
         const padding = () => {
             if (isWidthChanged) return '8px';
             if (!isClosedSidebar) return '18px';
         };
-        return getItem(label, key, <Icon />, { paddingLeft: padding() }, styles.link);
+        return getListItemWithIcon(label, key, <Icon />, { paddingLeft: padding() }, styles.link);
     });
     return arr;
 };
