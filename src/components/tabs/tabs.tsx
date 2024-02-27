e786483415813a33b72c2d9fe5d4dfad93e9a4d0
Katsiaryna Voronich <katarinka403@gmail.com>
2024-02-28 01:46:11 +0300

    feat: add tabs

---
+++ src/components/tabs/tabs.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Paths } from '@type/paths';
+import { REGISTRATION } from '@constants/index';
+import { Tabs } from 'antd';
+import { SignInForm } from '@components/form/sign-in-form';
+import { SignUpForm } from '@components/form/sign-up-form';
+import styles from './tabs.module.scss';
+
+export const AuthTabs: React.FC = () => {
+    const { pathname } = useLocation();
+    const signUpTab = 'registration';
+    const signInTab = 'auth';
+    const getActiveTab = () => (pathname.includes(signUpTab) ? signUpTab : signInTab);
+
+    return (
+        <Tabs
+            defaultActiveKey={signInTab}
+            className={styles.tabs}
+            activeKey={getActiveTab()}
+            items={[
+                {
+                    label: <Link to={Paths.AUTH}>Вход</Link>,
+                    key: signInTab,
+                    children: <SignInForm />,
+                },
+                {
+                    label: <Link to={REGISTRATION}>Регистрация</Link>,
+                    key: signUpTab,
+                    children: <SignUpForm />,
+                },
+            ]}
+        />
+    );
+};
