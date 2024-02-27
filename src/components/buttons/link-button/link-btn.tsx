2e31f5317b555aaf5f19a2716c1019d5cce02f8b
Katsiaryna Voronich <katarinka403@gmail.com>
2024-02-28 02:03:36 +0300

    refactor: update link button

--- src/components/buttons/link-button/link-btn.tsx
+++ src/components/buttons/link-button/link-btn.tsx
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, LinkProps } from 'react-router-dom';
 
 import styles from './link-btn.module.scss';
 
 type LinkButtonProps = {
     to: string;
+    state?: LinkProps['state'];
     icon?: JSX.Element;
     text?: string;
+    dataTestId?: string;
     className: string;
 };
 
-export const LinkButton = ({ to, icon, text, className }: LinkButtonProps) => (
-    <Link to={to} className={`${styles.link_btn} ${className}`}>
+export const LinkButton = ({ to, state, icon, text, dataTestId, className }: LinkButtonProps) => (
+    <Link
+        to={to}
+        className={`${styles.link_btn} ${className}`}
+        state={state}
+        data-test-id={dataTestId}
+    >
         {icon}
         {text}
     </Link>
