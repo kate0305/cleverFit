becc6557b9de1ead256b58aa490d4baa9cf29b93
Katsiaryna Voronich <katarinka403@gmail.com>
2024-02-27 23:04:27 +0300

    feat: implement slices

---
+++ src/redux/redusers/user-data-slice.ts
@@ -0,0 +1,50 @@
+import { RootState } from '@redux/configure-store';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ChangePasswordReq, UserReq } from '@type/service';
+import { getLocalStorageValue } from '@utils/use-local-storage';
+
+type UserDataReducerState = {
+    userData: UserReq | null;
+    changePasswordData: ChangePasswordReq | null;
+    email: string | null;
+    token: string | null;
+};
+
+const initialState: UserDataReducerState = {
+    userData: null,
+    changePasswordData: null,
+    email: null,
+    token: getLocalStorageValue('token', null),
+};
+
+export const userDataSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setSignUpData(state, action: PayloadAction<UserReq>) {
+            state.userData = action.payload;
+        },
+        setToken(state, action: PayloadAction<string>) {
+            state.token = action.payload;
+        },
+        setEmail(state, action: PayloadAction<string>) {
+            state.email = action.payload;
+        },
+        setChangePasswordData(state, action: PayloadAction<ChangePasswordReq>) {
+            state.changePasswordData = action.payload;
+        },
+        reset: () => {
+            return initialState;
+        },
+    },
+});
+
+export const { setSignUpData, setToken, setEmail, setChangePasswordData, reset } =
+    userDataSlice.actions;
+export const userDataReduser = userDataSlice.reducer;
+
+export const selectUserData = (state: RootState) => state.userDataReduser.userData;
+export const selectUserEmail = (state: RootState) => state.userDataReduser.email;
+export const selectToken = (state: RootState) => state.userDataReduser.token;
+export const selectChangePasswordData = (state: RootState) =>
+    state.userDataReduser.changePasswordData;
