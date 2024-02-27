985a054c5ca4c9f9ed34f8c15317d26a550b258f
Katsiaryna Voronich <katarinka403@gmail.com>
2024-02-27 11:13:33 +0300

    feat: add auth layout

---
+++ src/components/layouts/auth-layout/auth-layout.tsx
@@ -0,0 +1,26 @@
+import React, { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useAppSelector } from '@hooks/typed-react-redux-hooks';
+import { selectIsLoading } from '@redux/redusers/sign-up-slice';
+import { Loader } from '@components/loader';
+import classnames from 'classnames/bind';
+import styles from './auth-layout.module.scss';
+
+const cx = classnames.bind(styles);
+
+export const AuthLayout: React.FC = () => {
+    const isLoading = useAppSelector(selectIsLoading);
+
+    const className = cx({
+        wrapper: true,
+        filter: !isLoading,
+    });
+
+    return (
+        <div className={className}>
+            <Suspense fallback={<Loader />}>
+                <Outlet />
+            </Suspense>
+        </div>
+    );
+};
