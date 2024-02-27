ca90ac086017fb3627c312759b2792cfb8eeef18
Katsiaryna Voronich <katarinka403@gmail.com>
2024-02-28 01:50:03 +0300

    refactor: update main page

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
-import { Layout } from 'antd';
-
+import { Col, Layout, Row, Typography } from 'antd';
+import { ContentCard } from '@components/content-card';
+import { PossibilitieSection } from './content/possibilities-section';
+import { ActionsList } from './content/actions-list';
 import styles from './main-page.module.scss';
-import { Sidebar } from '@components/sidebar';
-import { AppHeader } from '@components/header';
-import { AppContent } from './layout';
-import { AppFooter } from '@components/footer';
 
-export const MainPage: React.FC = () => {
-    return (
-        <Layout className={styles.wrapper}>
-            <Sidebar />
-            <Layout style={{ background: 'transparent' }}>
-                <AppHeader />
-                <AppContent />
-                <AppFooter />
-            </Layout>
-        </Layout>
-    );
-};
+const { Content } = Layout;
+const { Paragraph } = Typography;
+
+export const MainPage: React.FC = () => (
+    <Content className={styles.content}>
+        <Row gutter={16} style={{ maxWidth: '768px' }}>
+            <Col span={24}>
+                <ContentCard
+                    className='main_card'
+                    bordered={false}
+                    content={<PossibilitieSection />}
+                />
+            </Col>
+        </Row>
+        <Row
+            gutter={[
+                { xs: 0, sm: 16 },
+                { xs: 6, sm: 16 },
+            ]}
+            style={{ maxWidth: '768px', marginTop: '24px' }}
+        >
+            <Col span={24}>
+                <ContentCard
+                    className='main_card'
+                    bordered={false}
+                    content={
+                        <Paragraph className={styles.text}>
+                            CleverFit — это не просто приложение, а твой личный помощник в&nbsp;мире
+                            фитнеса. Не откладывай на завтра — начни тренироваться уже сегодня!
+                        </Paragraph>
+                    }
+                />
+            </Col>
+        </Row>
+        <ActionsList />
+    </Content>
+);
