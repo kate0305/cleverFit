812a24c84453b398d8b8a90ba00cacfa23274869
Katsiaryna Voronich <katarinka403@gmail.com>
2024-03-06 00:58:00 +0300

    refactor: move google auth button to separate component

---
+++ src/components/buttons/google-auth-button/google-auth-btn.tsx
@@ -0,0 +1,24 @@
+import { GooglePlusOutlined } from '@ant-design/icons';
+
+import { BASE_URL, GOOGLE_AUTH } from '@constants/index';
+
+import { PrimaryBtn } from '../primary-button';
+
+import styles from './google-auth-btn.module.scss';
+
+
+export const GoogleAuthBtn = (props: { isSignUpForm?: boolean }) => {
+    const goToGoogleAuth = () => (window.location.href = `${BASE_URL}${GOOGLE_AUTH}`);
+
+    return (
+        <PrimaryBtn
+            type='default'
+            icon={<GooglePlusOutlined />}
+            htmlType='button'
+            btnText={props.isSignUpForm ? 'Регистрация через Google' : 'Войти через Google'}
+            className={props.isSignUpForm ? styles.google_reg : styles.google}
+            onClick={goToGoogleAuth}
+        />
+    );
+};
+
