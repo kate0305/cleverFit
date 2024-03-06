bd7b68e9fecd02f197d5ab99c3f3ad26d134f156
Katsiaryna Voronich <katarinka403@gmail.com>
2024-03-06 04:24:29 +0300

    feat: add modal window

---
+++ src/components/modal-window/modal-window.tsx
@@ -0,0 +1,54 @@
+import { CSSProperties, ReactNode } from 'react';
+import { Modal } from 'antd';
+
+import styles from './modal-window.module.scss';
+
+type ModalWindowgProps = {
+    children: ReactNode;
+    isOpen?: boolean;
+    closable?: boolean;
+    title?: ReactNode;
+    footer?: ReactNode;
+    okText?: string;
+    onOk?: () => void;
+    onCancel?: () => void;
+    maskStyle?: CSSProperties;
+    className?: string;
+};
+
+export const ModalWindow = ({
+    isOpen,
+    children,
+    closable,
+    title,
+    footer,
+    okText,
+    onOk,
+    onCancel,
+    maskStyle,
+    className,
+}: ModalWindowgProps) => {
+    return (
+        <Modal
+            centered
+            title={title}
+            open={isOpen}
+            closable={closable || false}
+            maskClosable={false}
+            footer={footer || null}
+            okText={okText}
+            width={539}
+            onOk={onOk}
+            onCancel={onCancel}
+            maskStyle={
+                maskStyle || {
+                    background: 'rgba(121, 156, 212, 0.50)',
+                    backdropFilter: 'blur(6px)',
+                }
+            }
+            className={className || styles.modal}
+            zIndex={1}
+            children={children}
+        />
+    );
+};
