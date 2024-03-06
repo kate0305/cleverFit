0dd64e1156dd9ab7012190127df7d4a67b77b3c9
Katsiaryna Voronich <katarinka403@gmail.com>
2024-03-06 04:41:56 +0300

    feat: add feedback page

---
+++ src/pages/feedbacks-page/feedback-container/feedbacks-container.tsx
@@ -0,0 +1,67 @@
+import { useMemo, useState } from 'react';
+
+import { Review } from '@type/feedbacks';
+
+import { PrimaryBtn } from '@components/buttons/primary-button';
+import { Feedback, NoFeedback } from '@components/feedbacks';
+
+import styles from './feedbacks-container.module.scss';
+
+type FeedbacksContainerProps = {
+    isHaveFeedbacks: boolean;
+    openReviewModal: () => void;
+    feedbacks?: Review[];
+};
+
+export const FeedbacksContainer = ({ feedbacks, isHaveFeedbacks, openReviewModal }: FeedbacksContainerProps) => {
+
+    const [isAllFeedbacks, toogleAllFeedbacks] = useState(false);
+    const toogleAmountFeedbacks = () => toogleAllFeedbacks(!isAllFeedbacks);
+
+    const sortedFeedbacks = useMemo(
+        () => feedbacks && [...feedbacks].sort((a, b) => b.createdAt.localeCompare(a.createdAt)),
+        [feedbacks],
+    );
+    const partOfFeedbacks = sortedFeedbacks?.slice(0, 4);
+    const feedbacksForRender = isAllFeedbacks ? sortedFeedbacks : partOfFeedbacks;
+
+    return (
+        <>
+            {!isHaveFeedbacks && <NoFeedback />}
+            {isHaveFeedbacks && (
+                <div className={styles.feedbacks}>
+                    {feedbacksForRender?.map(
+                        ({ id, fullName, imageSrc, message, rating, createdAt }) => (
+                            <Feedback
+                                key={id}
+                                fullName={fullName}
+                                image={imageSrc}
+                                message={message}
+                                rating={rating}
+                                date={createdAt}
+                            />
+                        ),
+                    )}
+                </div>
+            )}
+            <div className={isHaveFeedbacks ? styles.btn_group_has_feedback : styles.btn_group}>
+                <PrimaryBtn
+                    type='primary'
+                    btnText='Написать отзыв'
+                    className={styles.feedback_btn}
+                    dataTestId='write-review'
+                    onClick={openReviewModal}
+                />
+                {isHaveFeedbacks && (
+                    <PrimaryBtn
+                        type='text'
+                        btnText={isAllFeedbacks ? 'Свернуть все отзывы' : 'Развернуть все отзывы'}
+                        className={styles.all_feedbacks_btn}
+                        onClick={toogleAmountFeedbacks}
+                        dataTestId='all-reviews-button'
+                    />
+                )}
+            </div>
+        </>
+    );
+};
---
+++ src/pages/feedbacks-page/feedbacks-page.tsx
@@ -0,0 +1,97 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { reset } from '@redux/redusers/user-data-slice';
+import { useAppDispatch } from '@hooks/index';
+import { useGetFeedbackQuery } from '@services/feedbacks-service';
+
+import { errorHandler } from '@utils/error-handler';
+import { useLocalStorage } from '@utils/use-local-storage';
+
+import { Paths } from '@type/paths';
+import { ResultRequestKeys } from '@type/result-request-keys';
+import { StatusCode } from '@type/status-code';
+
+import { CreateReviewModal } from '@components/feedbacks/create-feedback-modal';
+import { ErrFeedbackModal } from '@components/feedbacks/err-feedback-modal';
+import { SuccessFeedbackModal } from '@components/feedbacks/success-feedback-modal/success-feedback-modal';
+import { ModalWindow } from '@components/modal-window';
+import { RequestResult } from '@components/request-result';
+
+import { FeedbacksContainer } from './feedback-container';
+
+import styles from './feedbacks-page.module.scss';
+
+export const FeedbacksPage = () => {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const [, , removeLocalStorageItem] = useLocalStorage('token', '');
+    const { data: feedbacks, error } = useGetFeedbackQuery();
+
+    const [isOpenCreateFeedback, setOpenCreateFeedback] = useState(false);
+    const [isErrModalOpen, setOpenErrModal] = useState(false);
+    const [isOpenSuccessPostModal, setOpenSuccessPostModal] = useState(false);
+    const [isOpenErrPostModal, setOpenErrPostModal] = useState(false);
+
+    const isHaveFeedbacks = Boolean(feedbacks && feedbacks.length > 0);
+
+    const openModalCreateFeedback = () => setOpenCreateFeedback(true);
+
+    const logOut = useCallback(() => {
+        dispatch(reset());
+        removeLocalStorageItem();
+        navigate(Paths.AUTH, { replace: true });
+    }, [dispatch, navigate, removeLocalStorageItem]);
+
+    useEffect(() => {
+        if (error) {
+            const err = errorHandler(error);
+            if (typeof err !== 'string' && err) {
+                const { errStatus } = err;
+                if (errStatus === StatusCode.FORBIDDEN) {
+                    logOut();
+                } else {
+                    setOpenErrModal(true);
+                }
+            }
+        }
+    }, [error, logOut]);
+
+    return (
+        <main className={isHaveFeedbacks ? styles.main : styles.main_with_feedbacks}>
+            <FeedbacksContainer
+                isHaveFeedbacks={isHaveFeedbacks}
+                feedbacks={feedbacks}
+                openReviewModal={openModalCreateFeedback}
+            />
+            {isErrModalOpen && (
+                <ModalWindow
+                    isOpen={isErrModalOpen}
+                    children={<RequestResult keyErr={ResultRequestKeys.GET_FEEDBACK_ERR} />}
+                />
+            )}
+            {isOpenCreateFeedback && (
+                <CreateReviewModal
+                    isOpen={isOpenCreateFeedback}
+                    setOpenModal={setOpenCreateFeedback}
+                    setOpenErrPostModal={setOpenErrPostModal}
+                    setOpenSuccessPostModal={setOpenSuccessPostModal}
+                />
+            )}
+            {isOpenSuccessPostModal && (
+                <SuccessFeedbackModal
+                    isOpen={isOpenSuccessPostModal}
+                    setOpenModal={setOpenSuccessPostModal}
+                />
+            )}
+            {isOpenErrPostModal && (
+                <ErrFeedbackModal
+                    isOpen={isOpenErrPostModal}
+                    setOpenModal={setOpenErrPostModal}
+                    setOpenCreateFeedback={setOpenCreateFeedback}
+                />
+            )}
+        </main>
+    );
+};
