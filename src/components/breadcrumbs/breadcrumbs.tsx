124e676f6941e8fa5e8ad0231f5d63c73c1c9f60
Katsiaryna Voronich <katarinka403@gmail.com>
2024-03-06 04:20:34 +0300

    refactor: update breadcrumbs

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { Breadcrumb } from 'antd';
+
+import { BREADCRUMB_NAMES } from '@constants/index';
 
 import styles from './breadcrumbs.module.scss';
 
-export const Breadcrumbs: React.FC = () => {
-    return (
-        <Breadcrumb className={styles.breadcrumbs}>
-            <Breadcrumb.Item>Главная</Breadcrumb.Item>
-        </Breadcrumb>
-    );
+export const Breadcrumbs = () => {
+    const location = useLocation();
+    const pathSnippets = location.pathname.split('/').filter((i) => i);
+
+    const extraBreadcrumbItems = pathSnippets.map((_, index) => {
+        const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+        return (
+            <Breadcrumb.Item key={url}>
+                <Link to={url}>{BREADCRUMB_NAMES[url]}</Link>
+            </Breadcrumb.Item>
+        );
+    });
+
+    const breadcrumbItems = [
+        <Breadcrumb.Item key='home'>
+            <Link to='/main'>Главная</Link>
+        </Breadcrumb.Item>,
+    ].concat(extraBreadcrumbItems);
+
+    return <Breadcrumb className={styles.breadcrumbs}>{breadcrumbItems}</Breadcrumb>;
 };
