14a79e83faa9f60f63bfaef25b140404b2cefcd1
Katsiaryna Voronich <katarinka403@gmail.com>
2024-03-22 14:21:34 +0300

    refactor: update project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'path';
-
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+    base: '/kate0305/',
     plugins: [react()],
     server: {
         host: true,
