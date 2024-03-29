55cef28f45ed093b9bb5af867b5de1ab1ee3a293
Katsiaryna Voronich <katarinka403@gmail.com>
2024-03-29 21:48:52 +0300

    refactor: delete base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/kate0305/',
     plugins: [react()],
     server: {
         host: true,
