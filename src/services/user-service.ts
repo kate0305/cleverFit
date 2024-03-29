68662e32eeda0acd1d8d52c965618c3d38bffa56
Katsiaryna Voronich <katarinka403@gmail.com>
2024-03-29 22:28:27 +0300

    refactor: apply eslint

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -6,73 +6,71 @@ import { commonOnQueryStarted } from '@utils/api-query-started';
 
 import { cleverFitApi } from './base-query';
 
-export const trainingApi = cleverFitApi.enhanceEndpoints({ addTagTypes: [Tags.USER] }).injectEndpoints({
-    endpoints: (builder) => ({
-        getUserData: builder.query<UserDataResp, void>({
-            query: () => USER_ME,
-            async onQueryStarted(_, { dispatch, queryFulfilled }) {
-                try {
-                    await commonOnQueryStarted({ dispatch, queryFulfilled }, true);
-                    const { data: userData } = await queryFulfilled;
+export const trainingApi = cleverFitApi
+    .enhanceEndpoints({ addTagTypes: [Tags.USER] })
+    .injectEndpoints({
+        endpoints: (builder) => ({
+            getUserData: builder.query<UserDataResp, void>({
+                query: () => USER_ME,
+                async onQueryStarted(_, { dispatch, queryFulfilled }) {
+                    try {
+                        await commonOnQueryStarted({ dispatch, queryFulfilled }, true);
+                        const { data: userData } = await queryFulfilled;
 
-                    dispatch(setUserData(userData));
-                } catch (e) {
-                    dispatch(setUserData(null));
-                }
-            },
-            providesTags: [Tags.USER],
-        }),
-
-        updateUserData: builder.mutation<UserDataResp, UserDataReq>({
-            query: (data) => ({
-                url: USER,
-                method: 'PUT',
-                body: data,
+                        dispatch(setUserData(userData));
+                    } catch (e) {
+                        dispatch(setUserData(null));
+                    }
+                },
+                providesTags: [Tags.USER],
             }),
-            async onQueryStarted(_, { dispatch, queryFulfilled }) {
-                try {
-                    await commonOnQueryStarted({ dispatch, queryFulfilled }, true);
-                    const { data: userData } = await queryFulfilled;
 
-                    dispatch(setUserData(userData));
-                } catch {
-                    dispatch(setUserData(null));
-                }
-            },
-            invalidatesTags: [Tags.USER],
-        }),
+            updateUserData: builder.mutation<UserDataResp, UserDataReq>({
+                query: (data) => ({
+                    url: USER,
+                    method: 'PUT',
+                    body: data,
+                }),
+                async onQueryStarted(_, { dispatch, queryFulfilled }) {
+                    try {
+                        await commonOnQueryStarted({ dispatch, queryFulfilled }, true);
+                        const { data: userData } = await queryFulfilled;
 
-        getTariffList: builder.query<TariffResp[], void>({
-            query: () => TARIFF_LIST,
-            async onQueryStarted(_, { dispatch, queryFulfilled }) {
-                try {
-                    await commonOnQueryStarted({ dispatch, queryFulfilled }, true);
-                    const { data: tariffList } = await queryFulfilled;
-
-                    dispatch(setTariffList(tariffList));
-                } catch (e) {
-                    console.log(e);
-                }
-            },
-        }),
-
-        payNewTariff: builder.mutation<void, PayNewTariffResp>({
-            query: (data) => ({
-                url: PAY_NEW_TARIFF,
-                method: 'POST',
-                body: data,
+                        dispatch(setUserData(userData));
+                    } catch {
+                        dispatch(setUserData(null));
+                    }
+                },
+                invalidatesTags: [Tags.USER],
             }),
-            async onQueryStarted(_, { dispatch, queryFulfilled }) {
-                try {
+
+            getTariffList: builder.query<TariffResp[], void>({
+                query: () => TARIFF_LIST,
+                async onQueryStarted(_, { dispatch, queryFulfilled }) {
+                    try {
+                        await commonOnQueryStarted({ dispatch, queryFulfilled }, true);
+                        const { data: tariffList } = await queryFulfilled;
+
+                        dispatch(setTariffList(tariffList));
+                    } catch (e) {
+                        dispatch(setTariffList([]));
+                    }
+                },
+            }),
+
+            payNewTariff: builder.mutation<void, PayNewTariffResp>({
+                query: (data) => ({
+                    url: PAY_NEW_TARIFF,
+                    method: 'POST',
+                    body: data,
+                }),
+                async onQueryStarted(_, { dispatch, queryFulfilled }) {
                     await commonOnQueryStarted({ dispatch, queryFulfilled }, true);
-                } catch (e) {
-                    console.log(e);
-                }
-            },
-            invalidatesTags: [Tags.USER],
+                },
+                invalidatesTags: [Tags.USER],
+            }),
         }),
-    }),
-});
+    });
 
 export const {
     useLazyGetUserDataQuery,
